ac018883f5701ad39da2371bc76ea0eef6e5097b
SergioOtavioTh <sergiootaviothomazferraz@gmail.com>
2025-10-16 16:24:39 -0300

    Trabalho Pre Lanchinho

--- EPA-2DS-2025/EPA-Principal/curso.php
+++ EPA-2DS-2025/EPA-Principal/curso.php
@@ -14,6 +14,7 @@ include 'header.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>EPA</title>
+    <link rel="stylesheet" href="cursoStyle.css">
 </head>
 <body>
     <div class="mb-3">
@@ -24,19 +25,19 @@ include 'header.php';
         </div>
         <div class="row">
             <p class="roboto-regular">
-            Curso de Desenvolvimento de Sistemas – ETEC Sales Gomes (Tatuí – SP) <br>
+                Curso de Desenvolvimento de Sistemas – ETEC Sales Gomes (Tatuí – SP) <br>
 
             O curso de Desenvolvimento de Sistemas integrado ao Ensino Médio da ETEC Sales Gomes é uma <br>
             oportunidade única para quem deseja começar o futuro agora. Além de cursar o ensino médio <br>
             normalmente, o estudante também aprende conteúdos técnicos voltados à tecnologia da informação, como <br>
             programação, banco de dados, criação de sites e aplicativos, e muito mais. <br>
             <br>
-            Ao final do curso, o aluno sai com diploma técnico reconhecido, preparado tanto para ingressar no <br>
+                Ao final do curso, o aluno sai com diploma técnico reconhecido, preparado tanto para ingressar no <br>
             mercado de trabalho quanto para continuar os estudos em faculdades da área. Enquanto muitos jovens <br>
             concluem apenas o ensino médio comum, quem estuda na ETEC já adquire experiência prática e <br>
             conhecimentos valorizados por empresas de tecnologia, que buscam cada vez mais profissionais qualificados. <br>
             <br>
-            Estudar na ETEC significa estar à frente, com ensino público de qualidade, professores especializados e uma <br>
+                Estudar na ETEC significa estar à frente, com ensino público de qualidade, professores especializados e uma <br>
             formação que une teoria, prática e inovação. Se você gosta de tecnologia, lógica e criatividade, o <br>
             Vestibulinho da ETEC Sales Gomes é o primeiro passo para transformar seu interesse em uma carreira promissora!
 
@@ -46,7 +47,19 @@ include 'header.php';
             <h1 class="subtitle">Creditos</h1>
         </div>
         <div class="row">
-            <p class="roboo-regular">[Inserir os integrantes de cada grupo. Por exemplo: Grupo 1: NOME 1, NOME 2 etc...]</p>
+            <p class="roboo-regular">
+                <ul>
+                    <li>Sergio Otavio Th. Ferraz (@sergiootavioth)</li>
+                    <li>Anthony Lebrão Silva</li>
+                    <li>Pedro Miguel Gallé</li>
+                    <li>Cauã Nanis Rosa</li>
+                    <li>Victor Moura</li>
+                    <li>Henrique R. Neves</li>
+                    <li>Matheus Ryan</li>
+                    <li>Luciano Souza</li>
+                    <li>Lucas Miranda</li>
+                </ul>
+            </p>
         </div>
     </div>
 </body>
--- EPA-2DS-2025/EPA-Principal/footer.php
+++ EPA-2DS-2025/EPA-Principal/footer.php
@@ -3,7 +3,7 @@
                  Se refere ao "crédito da classe"
                 -->
 
-<p class="fixed-bottom text-center roboto-title footer-bg">EPA - 2025 - 2DS</p>
+<p class="fixed-bottom text-center roboto-title footer-bg">© Todos os direitos reservados 2º Desenvolvimento de Sistemas - AMS</p>
 <script src="bootstrap.bundle.min.js"></script>
 <script src="main.js"></script>
 </body>
